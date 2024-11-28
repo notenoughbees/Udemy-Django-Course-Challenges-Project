76ebdda621715e63f9d11d786d03bc04119c388f
toomanybees <excessiveamountofbees@gmail.com>
2024-11-28 19:19:09 +1300

    27: redirects

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -16,8 +16,16 @@ monthly_challenges = {
     "december": "Learn Django for at least 20 minutes every day!"
 }
 
+
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+    
+    if month == 0 or month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
 
 def monthly_challenge(request, month):
     try:
@@ -25,4 +33,3 @@ def monthly_challenge(request, month):
         return HttpResponse(challenge_text)
     except:
         return HttpResponseNotFound("This month is not supported!")
-    
