2c49473c6a439e54bc3c0d6a19fa97b3f5a7f534
toomanybees <excessiveamountofbees@gmail.com>
2024-11-28 18:42:58 +1300

    23: add another view & url

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello world :-)")
+def january(request):
+    return HttpResponse("Eat no meat for the entire month!")
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes every day!")
+
+
