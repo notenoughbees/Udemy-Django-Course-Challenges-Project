389fc4da2e4e042d9304ef72f5ae706fc5c16306
toomanybees <excessiveamountofbees@gmail.com>
2024-11-28 18:53:37 +1300

    24b

--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes every day!")
-
 def monthly_challenge(request, month):
     challenge_text = None
     if(month == "january"):
