4cf1288fb0d44c73bb7fe1e1b9d488c5eadb22f2
toomanybees <excessiveamountofbees@gmail.com>
2024-11-28 19:39:44 +1300

    29: returning html

--- challenges/views.py
+++ challenges/views.py
@@ -22,7 +22,7 @@ def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     
     if month == 0 or month > len(months):
-        return HttpResponseNotFound("Invalid month")
+        return HttpResponseNotFound("<h3>Invalid month</h3>")
     
     redirect_month = months[month - 1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
@@ -32,6 +32,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h3>This month is not supported!</h3>")
