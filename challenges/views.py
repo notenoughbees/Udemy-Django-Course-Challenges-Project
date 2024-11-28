3ccfaccaaf24588e0f86e709512635e74bef3523
toomanybees <excessiveamountofbees@gmail.com>
2024-11-28 18:52:41 +1300

    24: dynamic path segments & captured values

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 def january(request):
     return HttpResponse("Eat no meat for the entire month!")
@@ -7,4 +7,32 @@ def january(request):
 def february(request):
     return HttpResponse("Walk for at least 20 minutes every day!")
 
-
+def monthly_challenge(request, month):
+    challenge_text = None
+    if(month == "january"):
+        challenge_text = "Eat no meat for the entire month!"
+    elif(month == "february"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "march"):
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    elif(month == "april"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "may"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "june"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "july"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "august"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "september"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "october"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "november"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif(month == "december"):
+        challenge_text = "Walk for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
