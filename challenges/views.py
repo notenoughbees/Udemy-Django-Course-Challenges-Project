4a5503cd0f2c392dbbce0ac6fb331552a15af0ce
toomanybees <excessiveamountofbees@gmail.com>
2024-11-28 18:59:55 +1300

    25: path converters

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
 def monthly_challenge(request, month):
     challenge_text = None
     if(month == "january"):
