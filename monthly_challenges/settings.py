60d74437f9ebc64cd9b55833eb51e1077c8563b0
toomanybees <excessiveamountofbees@gmail.com>
2024-11-29 21:05:15 +1300

    34: adding & registering templates

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = getenv("SECRET_KEY")
-SECRET_KEY = 'django-insecure-q)s5z$o_vbx@+*p+%ei#=9poke&pi04g3=cg@bu4a9f&d$=@@y'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'challenges' #register our app
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            #BASE_DIR / "challenges" / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
